b764f201629f6ce975930acd4811427d32e83fdc
Robert de Bock <robert@meinit.nl>
2022-06-24 15:37:37 +0200

    Fix syntax.

--- variables.tf
+++ variables.tf
@@ -12,9 +12,10 @@ variable "db_tcp_port" {
   validation {
     condition     = var.db_tcp_port > 0 && var.db_tcp_port < 65536 
     error_message = "Please choose a valid TCP port between 1 and 65535."
+  }
 }
 
 variable "db_username" {
-  description = "The username to use to connect to the database/"
+  description = "The username to use to connect to the database."
 }
 
