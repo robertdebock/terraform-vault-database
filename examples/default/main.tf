0a151256484dafa4c613fb7d01a933b8b67f8eaf
Robert de Bock <robert.debock@adfinis.com>
2024-08-27 09:23:36 +0200

    debug again.

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,11 +1,11 @@
-module "vault_db" {
-  source      = "../../"
-  db_username = "postgres"
-  db_password = "postgres"
-  db_hostname = "0.0.0.0"
-  db_tcp_port = 5432
-  depends_on = [
-    docker_container.vault,
-    docker_container.postgres
-  ]
-}
+# module "vault_db" {
+#   source      = "../../"
+#   db_username = "postgres"
+#   db_password = "postgres"
+#   db_hostname = "0.0.0.0"
+#   db_tcp_port = 5432
+#   depends_on = [
+#     docker_container.vault,
+#     docker_container.postgres
+#   ]
+# }
