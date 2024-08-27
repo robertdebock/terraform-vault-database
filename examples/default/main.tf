189319f69f6f6ef842da04e712ce3879b78f3961
Robert de Bock <robert.debock@adfinis.com>
2024-08-27 09:12:56 +0200

    Connect to 0.0.0.0

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -3,19 +3,19 @@ resource "docker_network" "default" {
   driver = "bridge"
 }
 
-# resource "time_sleep" "default" {
-#   create_duration = "30s"
-# }
+resource "time_sleep" "default" {
+  create_duration = "30s"
+}
 
-# module "vault_db" {
-#   source      = "../../"
-#   db_username = "postgres"
-#   db_password = "postgres"
-#   db_hostname = "postgres"
-#   db_tcp_port = 5432
-#   depends_on = [
-#     docker_container.vault,
-#     docker_container.postgres,
-#     time_sleep.default
-#   ]
-# }
+module "vault_db" {
+  source      = "../../"
+  db_username = "postgres"
+  db_password = "postgres"
+  db_hostname = "0.0.0.0"
+  db_tcp_port = 5432
+  depends_on = [
+    docker_container.vault,
+    docker_container.postgres,
+    time_sleep.default
+  ]
+}
